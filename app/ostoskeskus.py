f05723ff867e6345190b40f1ab9787e68f9e190a
Markus <markus.aurala@gmail.com>
2024-09-18 23:00:37 +0300

    The first take on styling (will need refactoring, though)

--- app/ostoskeskus.py
+++ app/ostoskeskus.py
@@ -16,4 +16,19 @@ def index():
     newest_products = db.execute(
         "SELECT product_id, user_id, name, description, price, quantity FROM Products WHERE quantity > 0 ORDER BY product_id DESC LIMIT 3;"
     ).fetchall()
-    return render_template("ostoskeskus/index.html", shops=newest_shops, products=newest_products)
+
+    # TODO: Get TOP x shops and products
+    popular_shops = []
+    popular_products = []
+
+    return render_template("ostoskeskus/index.html", newest_shops=newest_shops, newest_products=newest_products, popular_shops=popular_shops, popular_products=popular_products)
+
+
+@bp.route("/search", methods=("GET", "POST"))
+def search():
+    return redirect(url_for("ostoskeskus.products"))
+
+
+@bp.route("/products")
+def products():
+    return render_template("ostoskeskus/products.html")
