ede27224f4f18c539b6d0cf916dff7dbb8bd9d68
Markus <markus.aurala@gmail.com>
2024-09-28 15:11:20 +0300

    User profile updating

--- app/auth.py
+++ app/auth.py
@@ -21,7 +21,7 @@ def login_required(view):
     return wrapped_view
 
 
-@bp.route("/register", methods=("GET", "POST"))
+@bp.route("/register", methods=["GET", "POST"])
 def register():
     if request.method == "POST":
 
@@ -34,12 +34,12 @@ def register():
 
         if not request.form["email"]:
             error = "Sähköpostiosoite on pakollinen tieto."
-        elif not firstname or len(firstname) < 2:
-            error = "Etunimi on pakollinen tieto. Vähintään 2 merkkiä"
-        elif not lastname or len(lastname) < 2:
-            error = "Sukunimi on pakollinen tieto. Vähintään 2 merkkiä."
-        elif not password or len(password) < 6:
-            error = "Salasana on pakollinen tieto. Vähintään 6 merkkiä."
+        elif not firstname or len(firstname) < 2 or len(firstname) > 25:
+            error = "Etunimi on pakollinen tieto. 2-25 merkkiä"
+        elif not lastname or len(lastname) < 2 or len(lastname) > 25:
+            error = "Sukunimi on pakollinen tieto. 2-25 merkkiä."
+        elif not password or len(password) < 8 or len(password) > 32:
+            error = "Salasana on pakollinen tieto. 8-32 merkkiä."
 
         try:
             validated_email = validate_email(email, check_deliverability=False)
@@ -69,13 +69,41 @@ def register():
     return render_template("auth/register.html")
 
 
-@bp.route("/profile", methods=("GET", "POST"))
+@bp.route("/profile", methods=["GET", "POST"])
 @login_required
 def profile():
+
+    if request.method == "POST":
+
+        error = None
+
+        firstname = request.form["firstname"]
+        lastname = request.form["lastname"]
+
+        if not firstname or len(firstname) < 2 or len(firstname) > 25:
+            error = "Etunimi on pakollinen tieto. 2-25 merkkiä"
+        elif not lastname or len(lastname) < 2 or len(lastname) > 25:
+            error = "Sukunimi on pakollinen tieto. 2-25 merkkiä."
+
+        if error is None:
+            db = get_db()
+            db.execute(
+                "UPDATE Users SET firstname = ?, lastname = ? WHERE user_id = ?",
+                (firstname, lastname, g.user["user_id"]),
+            )
+            g.user = get_db().execute(
+                "SELECT user_id, firstname, lastname, email, is_admin FROM Users WHERE user_id = ?", (g.user["user_id"],)
+            ).fetchone()
+
+            flash("Tietojen päivitys onnistui.")
+            return render_template("auth/profile.html")
+
+        flash(error)
+
     return render_template("auth/profile.html")
 
 
-@bp.route("/login", methods=("GET", "POST"))
+@bp.route("/login", methods=["GET", "POST"])
 def login():
     if request.method == "POST":
         email = request.form["email"]
@@ -83,7 +111,7 @@ def login():
         db = get_db()
         error = None
         user = db.execute(
-            "SELECT * FROM Users WHERE email = ?", (email,)
+            "SELECT user_id, password FROM Users WHERE email = ?", (email,)
         ).fetchone()
 
         if user is None:
@@ -94,6 +122,7 @@ def login():
         if error is None:
             session.clear()
             session["user_id"] = user["user_id"]
+            session["cart"] = {}
             flash("Kirjautuminen onnistui.")
             return redirect(url_for("index"))
 
@@ -117,5 +146,5 @@ def load_logged_in_user():
         g.user = None
     else:
         g.user = get_db().execute(
-            "SELECT * FROM Users WHERE user_id = ?", (user_id,)
+            "SELECT user_id, firstname, lastname, email, is_admin FROM Users WHERE user_id = ?", (user_id,)
         ).fetchone()
