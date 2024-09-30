42ac95554f8f2b273c83f2970c4678ef33253f16
Markus <markus.aurala@gmail.com>
2024-09-30 17:21:46 +0300

    Product search

--- app/ostoskeskus.py
+++ app/ostoskeskus.py
@@ -24,22 +24,16 @@ def index():
     return render_template("ostoskeskus/index.html", newest_shops=newest_shops, newest_products=newest_products, popular_shops=popular_shops, popular_products=popular_products)
 
 
-@bp.route("/products", methods=["GET", "POST"])
+@bp.route("/products", methods=["GET"])
 def products():
-    if request.method == "POST":
-        search_term = request.form["search"]
-        if search_term is None:
-            return redirect(url_for("ostoskeskus.index"))
-
-        db = get_db()
-        filtered_products = db.execute(
-            "SELECT product_id, (SELECT name FROM Shops WHERE Shops.shop_id=Products.shop_id) AS shop_name, name, description, image, price, quantity FROM Products WHERE is_available=1 ORDER BY product_id DESC LIMIT 10;"
-        ).fetchall()
-        return render_template("ostoskeskus/products.html", products=filtered_products)
+        
+    search_term = request.args.get("search")
+    if search_term is None:
+        return redirect(url_for("ostoskeskus.index"))
 
     db = get_db()
-    all_products = db.execute(
-        "SELECT product_id, (SELECT name FROM Shops WHERE Shops.shop_id=Products.shop_id) AS shop_name, name, description, image, price, quantity FROM Products WHERE is_available=1 ORDER BY product_id DESC LIMIT 10;",
+    filtered_products = db.execute(
+        "SELECT product_id, (SELECT name FROM Shops WHERE Shops.shop_id=Products.shop_id) AS shop_name, name, description, image, price, quantity FROM Products WHERE name LIKE ? OR description LIKE ? AND is_available=1 ORDER BY product_id DESC LIMIT 10;",
+        ("%" + str(search_term) + "%", "%" + str(search_term) + "%"),
     ).fetchall()
-
-    return render_template("ostoskeskus/products.html", products=all_products)
+    return render_template("ostoskeskus/products.html", products=filtered_products)
